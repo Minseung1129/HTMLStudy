9b1e17a315cedeb778c1fed41a0fa637f80221b4
Minseung1129 <yuyoung1969@gmail.com>
2025-02-07 16:03:20 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.ask-btn').addEventListener('click', function() {
+    alert('문의하기 기능은 준비 중입니다!');
+});
+
+document.querySelector('.create-btn').addEventListener('click', function() {
+    alert('새 3D 맵을 생성하는 페이지로 이동합니다.');
+});
